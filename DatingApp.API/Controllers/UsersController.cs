304f5161149b9e8b04a2e0574dcbd851dd2c6646
Duncan Welch <dunc_nz@hotmail.com>
2020-05-11 11:06:09 +1000

    End of section 10

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using DatingApp.API.Data;
@@ -40,6 +41,26 @@ namespace DatingApp.API.Controllers
             var userToReturn = _mapper.Map<UserForDetailedDto>(user);
 
             return Ok(userToReturn);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userforUpdateDto)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+
+            var userFromRepo = await _repo.GetUser(id);
+
+            _mapper.Map(userforUpdateDto, userFromRepo);
+
+            if (await _repo.SaveAll())
+            {
+                return NoContent();
+            }
+
+            throw new System.Exception($"Updating user with {id} failed on save");
         }
     }
 }
